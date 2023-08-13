d70b4d68541a79abcada8bccb055c4b6609c6f4e
AlexFerJesus <alexfernandesdejesus.12@gmail.com>
2023-08-12 21:00:25 -0300

    teste2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// usarei as mesmas linhas de texto para fazer a Lição.
+Console.WriteLine("Meia noite. Um asuvio no ar,");
+Console.WriteLine("Nenhuma alma na estação. E o trem");
+Console.WriteLine("passou que eu nem vi.");
